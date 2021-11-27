087bdd9dcae3a81d27da13d626e112430998fd8e
RonaldJ3 <rrizzonj@gmail.com>
2021-11-26 17:01:13 -0800

    I finshed turtle color chooser and started crazy cat lady

--- _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
+++ _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
@@ -15,15 +15,45 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # TODO 1) Create a new Turtle
+    bob = turtle.Turtle()
+    bob.shape('turtle')
     #      2) Make the turtle draw a shape (this will take more than one line
     #         of code)
     #      3) Set the pen width to 10
+    bob.pensize(10)
+    for i in range(300):
     #      4) Ask the user what color pen they would like to draw with
+        r = simpledialog.askstring('pen color', "What color pen do you want?")
     #      5) Use an if/else statement to set the pen color that the user
     #         requested
+        if r == 'red':
+            bob.pencolor('red')
+            bob.pendown()
+            for i in range(4):
+                bob.forward(100)
+                bob.right(90)
+        elif r == 'blue':
+            bob.pencolor('blue')
+            bob.pendown()
+            for i in range(4):
+                bob.forward(100)
+                bob.right(90)
+        elif r == 'green':
+            bob.pencolor('green')
+            bob.pendown()
+            for i in range(4):
+                bob.forward(100)
+                bob.right(90)
+
+
     #      6) If the user doesn't enter anything, choose a random color
+        else:
+            bob.pencolor(get_random_color())
+            bob.pendown()
+            for i in range(4):
+                bob.forward(100)
+                bob.right(90)
     #      7) Put a loop around your code so that you keep asking the user for
     #         more colors & drawing them
-
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
--- _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
+++ _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
@@ -11,10 +11,17 @@ def play_video(url):
 
 if __name__ == '__main__':
     # TODO 1) Make a new window variable, window = Tk()
+    window = Tk()
     #      2) Hide the window using the window's .withdraw() method
+    window.withdraw()
     #      3) Ask the user how many cats they have
+    r = simpledialog.askstring('CatN', "how many cats do you have?")
     #      4) If they have 3 or more cats, tell them they are a crazy cat lady
+    if r == '3' :
+        messagebox('msg'"You are a crazy cat lady.")
     #      5) If they have less than 3 cats AND more than 0 cats, call the
+    elif r == '2' :
+        play_video("https://www.youtube.com/watch?v=cbP2N1BQdYc")
     #         play_video function below to show them a cat video
     #      6) If they have 0 cats, show them a video of A Frog Sitting on a
     #         Bench Like a Human
