f97ba3328733e15853c2990b173dcfb053ea06f1
RonaldJ3 <rrizzonj@gmail.com>
2022-02-04 18:27:51 -0800

    I finshed crazy cat lady and are you happy

--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -4,5 +4,20 @@ from tkinter import Tk, simpledialog, messagebox
 if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
+.k8    window = Tk()
+    window.withdraw()
 
+    h = simpledialog.askstring('AH', "Are you happy?")
+
+    if h == 'yes' :
+        messagebox.showinfo('AH', "Keep doing whatever your doing.")
+
+    elif h == 'no' :
+        H = simpledialog.askstring('AH', "Do you want to be happy?")
+
+        if H == 'yes' :
+            messagebox.showinfo('AH', "Change something.")
+
+        elif H == 'no' :
+            messagebox.showerror('AH', "Keep doing whatever your doing.")
     pass
