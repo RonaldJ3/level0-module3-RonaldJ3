6c298e1f61c2bac51943e3658a57ca974e3d9bc3
RonaldJ3 <rrizzonj@gmail.com>
2022-02-11 17:00:27 -0800

    I started own adventure

--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -4,7 +4,7 @@ from tkinter import Tk, simpledialog, messagebox
 if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
-.k8    window = Tk()
+    window = Tk()
     window.withdraw()
 
     h = simpledialog.askstring('AH', "Are you happy?")
--- _01_elif/_4_own_adventure/own_adventure.py
+++ _01_elif/_4_own_adventure/own_adventure.py
@@ -4,3 +4,16 @@ from tkinter import simpledialog, messagebox, Tk
 #  path of the plot.
 #  Use pop-ups, if statements, and your imagination to make an interesting
 #  story
+
+
+
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+
+    a = simpledialog.askstring('first choice', "You wake up in a dark cave with two tunnels. The tunnel on the left seems to gently slope up. While the tunnel on the right has a steep slope upwards and a small bit of light is coming from the other end of it.  ")
+
+
+
+
+
