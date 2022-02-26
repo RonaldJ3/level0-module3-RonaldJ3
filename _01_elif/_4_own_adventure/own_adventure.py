52dd53f4b262d770cf853586c5d39e225dde868d
RonaldJ3 <rrizzonj@gmail.com>
2022-02-25 17:00:51 -0800

    I countined own adventure

--- _01_elif/_4_own_adventure/own_adventure.py
+++ _01_elif/_4_own_adventure/own_adventure.py
@@ -16,6 +16,9 @@ if __name__ == '__main__':
     if a == 'left' :
         b = simpledialog.askstring('second choice', "You start to head done the left path. a little later you hear rocks falling back were you first came from. Will you go back and see what happened or countine down the ever so slightly brighter tunnel?")
     elif b == 'back' :
-        c = simpledialog.askstring('last choice', "after marching for a undetermined amount of time you start to feel tired and hungry. you think about sitting down and resting maybe even taking a nap. Will you rest or continue walking?")
+        c = simpledialog.askstring('last choice', "after walking for a undetermined amount of time you start to feel tired and hungry. you think about sitting down and resting maybe even taking a nap. Will you rest or continue walking?")
     elif c == 'continue' :
                 simpledialog.SimpleDialog('final', "you start to see some light. as the light gets closer you increase your speed. until you finaly get out of the cave you look around and notice a helicopter. you try to call for help but your voice fails and you collapse to the ground never to wake up again.")
+    elif a == 'right' :
+        d == simpledialog.askstring('second choice', "you slowly climb up the steep slope when you hear a faint rumble and look back to see the cave collapesing behind you start climbing as fast as you can. Sadly the rocks catch up with you and you are buried alive. will you try to dig your way out or stay still and wait for someone to save you?")
+    elif d == 'stay'
