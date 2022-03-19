6c6274a7801eed5a212159e0de043952bff915a4
RonaldJ3 <rrizzonj@gmail.com>
2022-03-18 17:03:48 -0700

    I finished own adventure

--- _01_elif/_4_own_adventure/own_adventure.py
+++ _01_elif/_4_own_adventure/own_adventure.py
@@ -15,11 +15,13 @@ if __name__ == '__main__':
 
     if a == 'left' :
         b = simpledialog.askstring('second choice', "You start to head done the left path. a little later you hear rocks falling back were you first came from. Will you go back and see what happened or countine down the ever so slightly brighter tunnel?")
-        if b == 'continue' :
+        if b == 'c' :
             c = simpledialog.askstring('last choice', "after walking for a undetermined amount of time you start to feel tired and hungry. you think about sitting down and resting maybe even taking a nap. Will you rest or continue walking?")
         if b == 'go back' :
             e = simpledialog.askstring('final', "you start walking back and the ceiling suddenly shakes. Ignoring the noise and slightly moving ground you keep walking twoards the place you were orignlly at. you finally see your starting point and notice the right tunnel caved in. glad you didn't try climbiming up that path. You turn and start walking back down the left tunnel. not long after the celing suddenly collapse above your head.")
-        if c == 'continue' :
+        if c == 'rest' :
+            simpledialog.SimpleDialog('final', "you sit down and take a break. After who knows how long you wake up to see s bright light. Once your eyes adjust you see a group of 5 men standing over you with ropes and the like. You weakly ask What took you so long?")
+        if c == 'c' :
             simpledialog.SimpleDialog('final', "you start to see some light. as the light gets closer you increase your speed. until you finaly get out of the cave you look around and notice a helicopter. you try to call for help but your voice fails and you collapse to the ground never to wake up again.")
     elif a == 'right' :
         d = simpledialog.askstring('second choice', "you slowly climb up the steep slope when you hear a faint rumble and look back to see the cave collapesing behind you start climbing as fast as you can. Sadly the rocks catch up with you and you are buried alive. will you try to dig your way out or stay still and wait for someone to save you?")
