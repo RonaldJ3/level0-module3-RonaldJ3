fa21a7462240f602308d38ec3ec2278e7001ff5b
RonaldJ3 <rrizzonj@gmail.com>
2022-06-10 16:56:25 -0700

    I started and finished shiny objects and walk of fame and started 99 bottles

--- _02_loop_variables/_1_shiny_objects/shiny_objects.py
+++ _02_loop_variables/_1_shiny_objects/shiny_objects.py
@@ -13,11 +13,12 @@ def play_mister_zee():
 
 def many_shiny_objects():
     # TODO 1) Call the method above to play Mister Zee
-    play_mister_zee("shiny-objects.wav")
+
     # TODO 2) Ask the user how many shiny objects they want
-
+    shiny = simpledialog.askstring('', "How many shiny objects do you want?")
     # TODO 3) Play the sound that many times
-
+    for i in range(int(shiny)):
+        play_mister_zee()
     pass
 
 
