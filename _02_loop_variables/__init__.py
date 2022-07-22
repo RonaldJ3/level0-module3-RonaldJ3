6394c4a833c66d00a3f5225eabec6591dfd6333c
RonaldJ3 <rrizzonj@gmail.com>
2022-07-22 16:58:53 -0700

    I finished 99 bottles and started bullseye

--- _02_loop_variables/__init__.py
+++ _02_loop_variables/__init__.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     window = Tk()
     window.withdraw()
     for i in range(98, 0, -1):
-        print(i+1 , 'bottles on the wall take one down pass it around ',i ,"bottles on the wall")
-    print
+        print(i+1 , 'bottles of beer on the wall take one down pass it around ',i ,"bottles on the wall")
+    print("No bottles of beer on the wall! No bottles of beer! Go to the store and buy some more 99 bottles of beer on the wall!")
