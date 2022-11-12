5be902bce9f98484f0f18513ace873e0e36904e3
RonaldJ3 <rrizzonj@gmail.com>
2022-11-11 18:23:53 -0800

    I countined fizzbuzz

--- _03_modulo/_4_fizz_buzz/fizz_buzz.py
+++ _03_modulo/_4_fizz_buzz/fizz_buzz.py
@@ -16,16 +16,7 @@
 if __name__ == '__main__':
     i = 1
     for i in range(20):
-        if i == 3:
-            print("fizz")
-        elif i >= 0:
+        if i >= 0:
             print(i + 1)
-
-
-
-
-
-
-
-
-
+        elif i % 3 == 0:
+            print("fizz")
